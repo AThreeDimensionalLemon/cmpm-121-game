13633b263371c121a7d1f25ef3ccc8cc5f8d1559
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-05-07 09:51:31 -0700

    Reapply "Created `Projectile` class to hold read JSON info"
    This reverts commit d57691eee601b2f4ac0bcd6d4eb5c2761752db95.

---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Projectile {
+    private ProjectileMovement trajectory;
+    private string speed;
+    private int sprite;
+}
