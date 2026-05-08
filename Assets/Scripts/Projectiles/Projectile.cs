ecf758bd984fbd949ba20eccb39e73412b92e989
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-05-08 14:07:24 -0700

    Made `Projectile` more compatible with what I'm about to do next

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -5,22 +5,8 @@ using System.Collections.Generic;
 using RPNEvaluator;
 
 public class Projectile {
-    private ProjectileMovement trajectory;
-    private int sprite;
-
-    public Projectile(JToken projectileToken) {
-
-        var childTokens = projectileToken.ToObject<Dictionary<string, string>>();
-
-        var rpnEvalDict = new Dictionary<string, float> {
-
-        };
-        float speed = RPNEvaluator.RPNEvaluator.Evaluatef(childTokens["speed"], rpnEvalDict);
-        float lifetime = (childTokens.ContainsKey("lifetime")) ? RPNEvaluator.RPNEvaluator.Evaluatef(childTokens["lifetime"], rpnEvalDict) : -1;
-
-        trajectory = (childTokens["trajectory"] == "straight") ? new StraightProjectileMovement(speed) :
-            (childTokens["trajectory"] == "homing") ? new HomingProjectileMovement(speed) :
-            new SpiralingProjectileMovement(speed);
-        sprite = projectileToken["sprite"].ToObject<int>();
-    }
+    public string trajectory;
+    public string speed;
+    public string lifetime;
+    public int sprite;
 }
