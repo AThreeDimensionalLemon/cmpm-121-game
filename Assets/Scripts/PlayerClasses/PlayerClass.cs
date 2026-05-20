48179c71026dac74ad734e281cccfebce840c8b7
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-05-20 11:19:32 -0700

    Added class for player classes

---
+++ Assets/Scripts/PlayerClasses/PlayerClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class PlayerClass {
+    public int sprite;
+    public string health;
+    public string mana;
+    public string mana_regeneration;
+    public string spellpower;
+    public string speed;
+}
