7aabae718d6c6514aebf3f85819f018086cb5da8
bagelcat11 <lynn.g0032@gmail.com>
2026-05-26 19:33:56 -0700

    spell ui highlight for active spell

--- Assets/Scripts/UI/SpellUIContainer.cs
+++ Assets/Scripts/UI/SpellUIContainer.cs
@@ -19,7 +19,12 @@ public class SpellUIContainer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // this absolutely should be a listener and not this loop in update -- lynn
+        if (GameManager.Instance.state != GameManager.GameState.PREGAME) {
+            for (int i = 0; i < 4; i++) {
+                spellUIs[i].GetComponent<SpellUI>().highlight.SetActive(player.spellcaster.current_spell_index == i);
+            }
+        }
     }
 
     public void ResetSpellUI()
