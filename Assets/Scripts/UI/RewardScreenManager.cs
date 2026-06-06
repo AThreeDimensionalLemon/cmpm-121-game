c61b9be5f99df9d8deb474002923f5e872b08429
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-06-06 14:17:13 -0700

    Remove fields of old spell and relic UI system and add field of new system

--- Assets/Scripts/UI/RewardScreenManager.cs
+++ Assets/Scripts/UI/RewardScreenManager.cs
@@ -15,18 +15,19 @@ public class RewardScreenManager : MonoBehaviour
     public GameObject statsReadout;
     public GameObject nextWaveButton;
     public GameObject restartButton;
-    public GameObject spellIconFrame;
-    public GameObject spellIcon;
-    public GameObject spellDescription;
-    public GameObject takeSpellButton;
-    private List<Relic> rewardRelics;
-    public List<GameObject> rewardRelicUIs;
-    public List<GameObject> rewardRelicDescriptions;
-    public List<GameObject> takeRelicButtons;
+    //public GameObject spellIconFrame;
+    //public GameObject spellIcon;
+    //public GameObject spellDescription;
+    //public GameObject takeSpellButton;
+    //private List<Relic> rewardRelics;
+    //public List<GameObject> rewardRelicUIs;
+    //public List<GameObject> rewardRelicDescriptions;
+    //public List<GameObject> takeRelicButtons;
     public SpellUIContainer spellUI;
+    [SerializeField] GameObject skillTree;
 
 
-    private ICastable rewardSpell;
+    //private ICastable rewardSpell;
     public enum TextTypes
     {
         POSTWAVE,
@@ -51,33 +52,33 @@ public class RewardScreenManager : MonoBehaviour
         switch(GameManager.Instance.state)
         {
             case GameManager.GameState.WAVEEND:
-                if (!rewardUI.activeSelf)
-                {
+                if (!rewardUI.activeSelf) {
+
                     SetRewardScreenText(TextTypes.POSTWAVE);
                     rewardUI.SetActive(true);
                     nextWaveButton.SetActive(true);
                     restartButton.SetActive(false);
 
-                    GenerateSpellReward();
-                    if (GameManager.Instance.player.GetComponent<PlayerController>().spellcaster.IsFull())
-                    {
-                        spellUI.ActivateDropButtons();
-                    }
-                    spellIconFrame.SetActive(true);
-                    spellIcon.SetActive(true);
-                    spellDescription.SetActive(true);
-                    takeSpellButton.SetActive(true);
-                    if (GameManager.Instance.GetWave() % 3 == 0)
-                    {
-                        GenerateRelicRewards();
-                    }
-                    else
-                    {
-                        for (int i = 0; i < rewardRelicUIs.Count; i++)
-                        {
-                            rewardRelicUIs.ElementAt(i).transform.parent.GameObject().SetActive(false);
-                        }
-                    }
+                    //GenerateSpellReward();
+                    //if (GameManager.Instance.player.GetComponent<PlayerController>().spellcaster.IsFull())
+                    //{
+                    //    spellUI.ActivateDropButtons();
+                    //}
+                    //spellIconFrame.SetActive(true);
+                    //spellIcon.SetActive(true);
+                    //spellDescription.SetActive(true);
+                    //takeSpellButton.SetActive(true);
+                    //if (GameManager.Instance.GetWave() % 3 == 0)
+                    //{
+                    //    GenerateRelicRewards();
+                    //}
+                    //else
+                    //{
+                    //    for (int i = 0; i < rewardRelicUIs.Count; i++)
+                    //    {
+                    //        rewardRelicUIs.ElementAt(i).transform.parent.GameObject().SetActive(false);
+                    //    }
+                    //}
                 }
                 break;
             case GameManager.GameState.GAMEOVER:
@@ -88,10 +89,10 @@ public class RewardScreenManager : MonoBehaviour
                     nextWaveButton.SetActive(false);
                     restartButton.SetActive(true);
 
-                    spellIconFrame.SetActive(false);
-                    spellIcon.SetActive(false);
-                    spellDescription.SetActive(false);
-                    takeSpellButton.SetActive(false);
+                    //spellIconFrame.SetActive(false);
+                    //spellIcon.SetActive(false);
+                    //spellDescription.SetActive(false);
+                    //takeSpellButton.SetActive(false);
                 }
                 break;
             case GameManager.GameState.GAMELOST:
@@ -103,10 +104,10 @@ public class RewardScreenManager : MonoBehaviour
                     restartButton.SetActive(true);
                     GameManager.Instance.KillAllEnemies();
 
-                    spellIconFrame.SetActive(false);
-                    spellIcon.SetActive(false);
-                    spellDescription.SetActive(false);
-                    takeSpellButton.SetActive(false);
+                    //spellIconFrame.SetActive(false);
+                    //spellIcon.SetActive(false);
+                    //spellDescription.SetActive(false);
+                    //takeSpellButton.SetActive(false);
                 }
                 break;
             default:
@@ -119,50 +120,48 @@ public class RewardScreenManager : MonoBehaviour
         }
     }
 
-    void GenerateSpellReward()
-    {
-        Dictionary<string, int> RPNDict = new();
-        RPNDict.Add("wave", GameManager.Instance.GetWave());
-        rewardSpell = SpellBuilder.Instance.BuildRandomSpell(GameManager.Instance.player.GetComponent<PlayerController>().spellcaster, "wave 2 / 1 +", RPNDict);
-        GameManager.Instance.spellIconManager.PlaceSprite(rewardSpell.GetIcon(), spellIcon.GetComponent<Image>());
-        spellDescription.GetComponent<TextMeshProUGUI>().text = rewardSpell.GetName() + "\n\n" + rewardSpell.GetDescription();
-    }
+    //void GenerateSpellReward()
+    //{
+    //    Dictionary<string, int> RPNDict = new();
+    //    RPNDict.Add("wave", GameManager.Instance.GetWave());
+    //    rewardSpell = SpellBuilder.Instance.BuildRandomSpell(GameManager.Instance.player.GetComponent<PlayerController>().spellcaster, "wave 2 / 1 +", RPNDict);
+    //    GameManager.Instance.spellIconManager.PlaceSprite(rewardSpell.GetIcon(), spellIcon.GetComponent<Image>());
+    //    spellDescription.GetComponent<TextMeshProUGUI>().text = rewardSpell.GetName() + "\n\n" + rewardSpell.GetDescription();
+    //}
 
     public void GiveSpellToPlayer()
     {
-        if (GameManager.Instance.player.GetComponent<PlayerController>().AddNewSpell(rewardSpell))
-        {
-            takeSpellButton.SetActive(false);
-            spellUI.DeactivateDropButtons();
-        }
+        //if (GameManager.Instance.player.GetComponent<PlayerController>().AddNewSpell(rewardSpell))
+        //{
+        //    takeSpellButton.SetActive(false);
+        //    spellUI.DeactivateDropButtons();
+        //}
     }
 
-    void GenerateRelicRewards()
-    {
-        rewardRelics = new();
-        for(int i = 0; i < rewardRelicUIs.Count; i++)
-        {
-            Relic reward = RelicManager.Instance.GetRandomRelic();
-            Debug.Log(reward.ToString());
-            rewardRelics.Add(reward);
-            rewardRelicUIs.ElementAt(i).GetComponent<RelicUI>().Instantiate(reward);
-            rewardRelicDescriptions.ElementAt(i).GetComponent<TextMeshProUGUI>().text = "\t\t" + reward.name + "\n\n\n\n" + reward.trigger.description + ",\n" + reward.effect.description;
-            rewardRelicUIs.ElementAt(i).transform.parent.GameObject().SetActive(true);
-        }
-    }
+    //void GenerateRelicRewards()
+    //{
+    //    rewardRelics = new();
+    //    for(int i = 0; i < rewardRelicUIs.Count; i++)
+    //    {
+    //        Relic reward = RelicManager.Instance.GetRandomRelic();
+    //        Debug.Log(reward.ToString());
+    //        rewardRelics.Add(reward);
+    //        rewardRelicUIs.ElementAt(i).GetComponent<RelicUI>().Instantiate(reward);
+    //        rewardRelicDescriptions.ElementAt(i).GetComponent<TextMeshProUGUI>().text = "\t\t" + reward.name + "\n\n\n\n" + reward.trigger.description + ",\n" + reward.effect.description;
+    //        rewardRelicUIs.ElementAt(i).transform.parent.GameObject().SetActive(true);
+    //    }
+    //}
 
     public void GiveRelicToPlayer(int index)
     {
-        for (int i = 0; i < rewardRelicUIs.Count; i++)
-        {
-            rewardRelicUIs.ElementAt(i).transform.parent.GameObject().SetActive(false);
-            RelicManager.Instance.AddRelicToUnowned(rewardRelics.ElementAt(i).name);
-            if (i == index)
-            {
-                EventBus.Instance.TakeRelic(RelicManager.Instance.GetRelic(rewardRelics.ElementAt(i).name));
-            }
-        }
-        rewardRelics.Clear();
+        //for (int i = 0; i < rewardRelicUIs.Count; i++) {
+        //    rewardRelicUIs.ElementAt(i).transform.parent.GameObject().SetActive(false);
+        //    RelicManager.Instance.AddRelicToUnowned(rewardRelics.ElementAt(i).name);
+        //    if (i == index) {
+        //        EventBus.Instance.TakeRelic(RelicManager.Instance.GetRelic(rewardRelics.ElementAt(i).name));
+        //    }
+        //}
+        //rewardRelics.Clear();
     }
 
     void SetRewardScreenText(TextTypes in_text)
@@ -175,6 +174,4 @@ public class RewardScreenManager : MonoBehaviour
             tmp.text += stats.GetStatisticsReadout();
         }
     }
-
-    
 }
