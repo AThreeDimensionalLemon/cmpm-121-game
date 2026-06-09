aa9e2eb915013297565677f2547aed0057ded582
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-06-08 21:08:08 -0700

    Add node field to tree node buttons

--- Assets/Scripts/UI/TreeSelectorController.cs
+++ Assets/Scripts/UI/TreeSelectorController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 class TreeSelectorController : MenuSelectorController {
+    SkillTreeNode node;
 
-    public void Setup(string text) {
+    public void Setup(string text, SkillTreeNode inNode) {
         label.text = text;
+        node = inNode;
     }
 
     public void ExecuteTask() {
