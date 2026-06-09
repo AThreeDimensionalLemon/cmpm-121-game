5d84cd7c536806d06937c1f6bddefde35e2826c6
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-06-09 15:14:04 -0700

    Add spell cast playing

---
+++ Assets/Scripts/Core/SfxManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour {
+    [SerializeField] GameObject player;
+    Action<SpellCaster> OnSpellCast;
+
+    void Start() {
+        EventBus eventBus = EventBus.Instance;
+
+        OnSpellCast = (caster) => {
+            player.GetComponent<AudioSource>().Play();
+        };
+        eventBus.OnSpellCast += OnSpellCast;
+    }
+}
