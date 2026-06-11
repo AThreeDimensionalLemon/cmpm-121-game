1b93e5e5871eac41756a76096eef25965017e480
qk9 <iain6475@gmail.com>
2026-06-10 19:51:06 -0700

    Disallowed player casting spells in the wave end screen

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -147,7 +147,7 @@ public class PlayerController : MonoBehaviour
 
     void OnAttack(InputValue value)
     {
-        if (GameManager.Instance.state == GameManager.GameState.PREGAME || GameManager.Instance.state == GameManager.GameState.GAMEOVER || GameManager.Instance.state == GameManager.GameState.GAMELOST) return;
+        if (GameManager.Instance.state != GameManager.GameState.INWAVE && GameManager.Instance.state != GameManager.GameState.COUNTDOWN) return;
         Vector2 mouseScreen = Mouse.current.position.value;
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
         mouseWorld.z = 0;
